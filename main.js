8f24b4f3c9c062c6a1d3ad7234bcab774d19807e
Juan David Paniagua Cano <david0613pc@gmail.com>
2022-07-02 17:02:32 -0500

    first_commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function edad(age = prompt("Ingrese su edad")) {
+  parseInt(edad);
+  if (edad >= 18) {
+    document.write(`Eres mayor de edad tienes ${edad} años`);
+  } else if (edad > 0) {
+    document.write(`Eres menor de edad tienes ${edad} años`);
+  } else {
+    document.write(`Ingresaste un valor invalido`);
+  }
+}
+edad();
+
+function salario(hour = parseFloat(prompt("Ingrese sus horas trabajadas"))) {
+  const SAL = 50;
+  let pago = hour * SAL;
+  document.write(`Su salario es de: ${pago}`);
+}
+
+salario();
+
+arreglo = [1, 23, 4, 5, 6];
+arreglo.map((element) => console.log(element * 2));
+arreglo.find((element) => console.log(element));
+
+const objeto = {
+  name: "manolina",
+};
+
+const objeto2 = {
+  name: "manolina",
+};
